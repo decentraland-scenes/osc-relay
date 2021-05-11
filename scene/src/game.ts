f92bf199188cf5c25b6ca296ecfc7ea7d2f86535
Nicolas Earnshaw <nearnshaw@decentraland.org>
2021-05-11 16:19:13 -0300

    color blocks

--- scene/src/game.ts
+++ scene/src/game.ts
@@ -6,21 +6,58 @@ connect('my_room').then((room) => {
   log('Connected!')
 
   room.state.listen('fader1', (value: number) => {
-    fader1.getComponent(Transform).position.y = value * 10 + 2
+    fader1.getComponent(Transform).position.y = value * 5 + 2
+    if(value> 0.9){
+      fader1.addComponentOrReplace(redMaterial)
+    } else if (value > 0.6){
+      fader1.addComponentOrReplace(yellowMaterial)
+    } else{
+      fader1.addComponentOrReplace(greenMaterial)
+    }
   })
 
   room.state.listen('fader2', (value: number) => {
-    fader2.getComponent(Transform).position.y = value * 10 + 2
+    fader2.getComponent(Transform).position.y = value * 5 + 2
+    if(value> 0.9){
+      fader2.addComponentOrReplace(redMaterial)
+    } else if (value > 0.6){
+      fader2.addComponentOrReplace(yellowMaterial)
+    } else{
+      fader2.addComponentOrReplace(greenMaterial)
+    }
   })
 
   room.state.listen('fader3', (value: number) => {
-    fader3.getComponent(Transform).position.y = value * 10 + 2
+    fader3.getComponent(Transform).position.y = value * 5 + 2
+    if(value> 0.9){
+      fader3.addComponentOrReplace(redMaterial)
+    } else if (value > 0.6){
+      fader3.addComponentOrReplace(yellowMaterial)
+    } else{
+      fader3.addComponentOrReplace(greenMaterial)
+    }
   })
 
   room.state.listen('fader4', (value: number) => {
-    fader4.getComponent(Transform).position.y = value * 10 + 2
+    fader4.getComponent(Transform).position.y = value * 5 + 2
+    if(value> 0.9){
+      fader4.addComponentOrReplace(redMaterial)
+    } else if (value > 0.6){
+      fader4.addComponentOrReplace(yellowMaterial)
+    } else{
+      fader4.addComponentOrReplace(greenMaterial)
+    }
   })
 })
+
+let greenMaterial = new Material()
+greenMaterial.albedoColor = Color4.Green()
+
+let yellowMaterial = new Material()
+yellowMaterial.albedoColor = Color4.Yellow()
+
+let redMaterial = new Material()
+redMaterial.albedoColor = Color4.Red()
 
 let fader1 = new Entity()
 fader1.addComponent(new BoxShape())
@@ -29,6 +66,7 @@ fader1.addComponent(
     position: new Vector3(8, 2, 2),
   })
 )
+fader1.addComponent(greenMaterial)
 engine.addEntity(fader1)
 
 let fader2 = new Entity()
@@ -38,6 +76,7 @@ fader2.addComponent(
     position: new Vector3(8, 2, 4),
   })
 )
+fader2.addComponent(greenMaterial)
 engine.addEntity(fader2)
 
 let fader3 = new Entity()
@@ -47,6 +86,7 @@ fader3.addComponent(
     position: new Vector3(8, 2, 6),
   })
 )
+fader3.addComponent(greenMaterial)
 engine.addEntity(fader3)
 
 let fader4 = new Entity()
@@ -56,4 +96,5 @@ fader4.addComponent(
     position: new Vector3(8, 2, 8),
   })
 )
+fader4.addComponent(greenMaterial)
 engine.addEntity(fader4)
